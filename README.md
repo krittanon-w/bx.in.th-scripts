a26b9b542e669c5e8c7f49d8bbaa4f6ab59d69d1
Krittanon Wisedchart <Krittanon.w@gmail.com>
2018-07-08 06:40:17 +0700

    first commit 😎

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bx.in.th-style
 Tampermonkey script for edit bx.in.th (Bitcoin centralize exchange)
+
+### How to use
+1. install Tampermonkey (Chrome Extension)
+2. copy script & paste
+3. enable and run script
+
+### Original vs new style
---
+++ script.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         bx_ui
+// @namespace    bx
+// @version      1.0
+// @description  new Bx.in.th UI
+// @author       Krittanon Wisedchart
+// @match        https://bx.in.th/THB/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    // config
+    var autoStyleListVolume = 400000; // more than 0 is auto
+    var styleList = [
+        {volume: 400000, color: "#CA92EC"},
+        {volume: 200000, color: "#5C9DED"},
+        {volume: 100000, color: "#ED5564"},
+        {volume: 50000, color: "#F87F52"},
+        {volume: 25000, color: "#FCCE54"},
+    ];
+    var intervalTime = 200 // // refresh interval time in ms
+
+    // main
+    if(autoStyleListVolume > 0) {
+        let count = 0;
+        for(let style of styleList){
+            style.volume = autoStyleListVolume/(Math.pow(2,count++));
+        }
+    }
+
+    // remove short table
+    $('.tradetbls div.panel-content').each(function(){
+        $(this).css("height", "1200px");
+    });
+
+    // remove scrollbar
+    $('.scrollbar').each(function(){
+        $(this).perfectScrollbar('destroy');
+    });
+
+    // scan tables
+    setInterval(function(){
+        let tables = $('.tradetbls .span4 table').toArray();
+        for(let [tableIndex, table] of tables.entries()){
+            // only sell and buy table
+            if(tableIndex !== 0){
+                const rows = $(table).children("tbody").children("tr").toArray();
+                for(let [rowIndex, row] of rows.entries()){
+                    // filter only text
+                    // 12,000 THB 12 1,000 coin -> 12000 THB 12 1000 coin -> [12000, THB, 12, 1000, coin]
+                    let text = row.outerText.replace(/,/g, '').split(/\t|\s/);
+
+                    // rate * coin
+                    let volume = parseFloat(text[text.length-3]) * parseFloat(text[text.length-2]);
+
+                    for(let [styleIndex, style] of styleList.entries()){
+                        if(volume >= style.volume){
+                            let cols = $(row).children("td").toArray();
+                            for(let [colIndex, col] of cols.entries()){
+                                $(col).attr("style",`background-color: ${style.color} !important`);
+                            };
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }, intervalTime);
+})();
